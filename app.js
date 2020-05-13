2c79d5863e7194d80b54d9b940240e96ea24011b
ETomacheski <tomacheski2001@gmail.com>
2020-05-13 09:29:01 -0300

    add get /

--- app.js
+++ app.js
@@ -6,17 +6,21 @@ const server = http.createServer(app);
 const socketIo = require('socket.io')
 
 const io = socketIo.listen(server) 
-
-server.listen(3000, ()=>{
-    console.log("running")
-})
-
 app.use(express.static(__dirname + "/public"));
+
+
+app.get('/',(req,res)=>{
+    console.log("acessou por aqui")
+    res.render('index.js')
+})
 const historico = [];
 io.on('connection',(socket)=>{
 
     console.log('Nova conexão');
     
+
+    
+
     historico.forEach(linha => {
         socket.emit('desenhar',linha)
     });
@@ -33,4 +37,8 @@ io.on('connection',(socket)=>{
         io.emit('desenhar')
       })
 
+})
+
+server.listen(3000, ()=>{
+    console.log("running")
 })
